60857469a1da593734dbcdf25965b8f626484100
navidaziz98 <navidaziz98@gmail.com>
2023-04-13 01:33:17 +0500

    miner bug

--- application/views/online_cases/requests.php
+++ application/views/online_cases/requests.php
@@ -2,97 +2,99 @@
 <h4> <?php echo  $title;  ?>
   <span class="label label-primary pull-right"><?php echo sizeof($requests); ?></span>
 </h4>
-<table class="table table-bordered table_small" id="<?php echo  str_replace(" ", "_", $title);  ?>" style="font-size:11px">
-  <thead>
-    <tr>
-      <th>#</th>
+<div class="table-responsive">
+  <table class="table table-bordered table_small" id="<?php echo  str_replace(" ", "_", $title);  ?>" style="font-size:11px">
+    <thead>
+      <tr>
+        <th>#</th>
 
-      <th>SID</th>
-      <th>School Name</th>
-      <th>District</th>
-      <th>Session</th>
-      <th>Days</th>
-      <th>Defic</th>
-      <th>Note</th>
-      <th>Fine</th>
-      <th>Action</th>
+        <th>SID</th>
+        <th>School Name</th>
+        <th>District</th>
+        <th>Session</th>
+        <th>Days</th>
+        <th>Defic</th>
+        <th>Note</th>
+        <th>Fine</th>
+        <th>Action</th>
 
-    </tr>
-  </thead>
-  <tbody>
-    <?php
-    $count = 1;
-    $previous_school_id = 0;
-    foreach ($requests as $request) {
-      if ($request->previous_session_status != 8) {
-    ?>
+      </tr>
+    </thead>
+    <tbody>
+      <?php
+      $count = 1;
+      $previous_school_id = 0;
+      foreach ($requests as $request) {
+        if ($request->previous_session_status != 8) {
+      ?>
 
-        <?php if ($request->deficient == 0  or 1 == 1) { ?>
-          <tr <?php if ($request->deficient > 0) { ?> style="color:red; <?php if ($list_type == 1) { ?> display:none;<?php } ?> " <?php } ?>>
+          <?php if ($request->deficient == 0  or 1 == 1) { ?>
+            <tr <?php if ($request->deficient > 0) { ?> style="color:red; <?php if ($list_type == 1) { ?> display:none;<?php } ?> " <?php } ?>>
 
-            <?php if ($previous_school_id != $request->schools_id) { ?>
-              <td><?php echo $count++; ?> </td>
+              <?php if ($previous_school_id != $request->schools_id) { ?>
+                <td><?php echo $count++; ?> </td>
 
-              <td><?php echo $request->schools_id ?></td>
-              <td><?php echo substr($request->schoolName, 0, 45) ?></td>
-              <td><?php echo $request->districtTitle; ?></td>
-            <?php } else { ?>
-              <td colspan="4"></td>
-              <td style="display: none;"></td>
-              <td style="display: none;"></td>
-              <td style="display: none;"></td>
-            <?php } ?>
-            <td><?php echo $request->sessionYearTitle ?></td>
-            <td style="text-align: center;" title="<?php echo date('d M, Y', strtotime($request->apply_date)); ?>">
-              <?php
-              //strtotime($request->apply_date)
-              if ($request->apply_date) {
-                echo timeago(strtotime($request->apply_date));
-              }
-              ?></td>
-            <td style="text-align: center;">
-              <?php
-              $query = "SELECT COUNT(*) as total FROM `file_status_logs` WHERE `file_status` = 5 and schools_id = '" . $request->schools_id . "'";
-              $once_deficient = $this->db->query($query)->row()->total;
-              if ($once_deficient > 0) {
-                echo '<i title="Deficiency completed" class="fa fa-flag" style="color:red" aria-hidden="true">1</i>';
-              }
-              ?>
-            </td>
-            <td style="text-align: center;">
-              <?php
-              $query = "SELECT COUNT(*) as total FROM `comments` WHERE school_id='" . $request->school_id . "' and schools_id = '" . $request->schools_id . "' and deleted=0";
-              $comments = $this->db->query($query)->row()->total;
-              if ($comments > 0) {
-                echo '<i title="Maybe notesheet completed" class="fa fa-comment" style="color:green" aria-hidden="true">1</i>';
-              }
-              ?>
-            </td>
-            <td style="text-align: center;">
-              <?php if ($request->isfined == 1) { ?>
-                <i class="fa fa-ban" title="Fine on this school" style="color: red;">1</i>
-              <?php } ?>
-            </td>
-
-
-            <td>
-              <?php if ($request->registrationNumber) { ?>
-                <!--<a href="<?php echo site_url("online_cases/combine_note_sheet/$request->schools_id"); ?>">Notesheet</a>-->
-                <a href="javascript:void(0)"><span onclick="search('<?php echo $request->schools_id; ?>')">Notesheet</span></a>
+                <td><?php echo $request->schools_id ?></td>
+                <td><?php echo substr($request->schoolName, 0, 45) ?></td>
+                <td><?php echo $request->districtTitle; ?></td>
               <?php } else { ?>
-                <a href="<?php echo site_url("online_cases/single_note_sheet/$request->schools_id/$request->school_id"); ?>"> New Registration </a>
+                <td colspan="4"></td>
+                <td style="display: none;"></td>
+                <td style="display: none;"></td>
+                <td style="display: none;"></td>
               <?php } ?>
-            </td>
+              <td><?php echo $request->sessionYearTitle ?></td>
+              <td style="text-align: center;" title="<?php echo date('d M, Y', strtotime($request->apply_date)); ?>">
+                <?php
+                //strtotime($request->apply_date)
+                if ($request->apply_date) {
+                  echo timeago(strtotime($request->apply_date));
+                }
+                ?></td>
+              <td style="text-align: center;">
+                <?php
+                $query = "SELECT COUNT(*) as total FROM `file_status_logs` WHERE `file_status` = 5 and schools_id = '" . $request->schools_id . "'";
+                $once_deficient = $this->db->query($query)->row()->total;
+                if ($once_deficient > 0) {
+                  echo '<i title="Deficiency completed" class="fa fa-flag" style="color:red" aria-hidden="true">1</i>';
+                }
+                ?>
+              </td>
+              <td style="text-align: center;">
+                <?php
+                $query = "SELECT COUNT(*) as total FROM `comments` WHERE school_id='" . $request->school_id . "' and schools_id = '" . $request->schools_id . "' and deleted=0";
+                $comments = $this->db->query($query)->row()->total;
+                if ($comments > 0) {
+                  echo '<i title="Maybe notesheet completed" class="fa fa-comment" style="color:green" aria-hidden="true">1</i>';
+                }
+                ?>
+              </td>
+              <td style="text-align: center;">
+                <?php if ($request->isfined == 1) { ?>
+                  <i class="fa fa-ban" title="Fine on this school" style="color: red;">1</i>
+                <?php } ?>
+              </td>
 
 
-          </tr>
-        <?php } ?>
-    <?php
-        $previous_school_id =  $request->schools_id;
-      }
-    } ?>
-  </tbody>
-</table>
+              <td>
+                <?php if ($request->registrationNumber) { ?>
+                  <!--<a href="<?php echo site_url("online_cases/combine_note_sheet/$request->schools_id"); ?>">Notesheet</a>-->
+                  <a href="javascript:void(0)"><span onclick="search('<?php echo $request->schools_id; ?>')">Notesheet</span></a>
+                <?php } else { ?>
+                  <a href="<?php echo site_url("online_cases/single_note_sheet/$request->schools_id/$request->school_id"); ?>"> New Registration </a>
+                <?php } ?>
+              </td>
+
+
+            </tr>
+          <?php } ?>
+      <?php
+          $previous_school_id =  $request->schools_id;
+        }
+      } ?>
+    </tbody>
+  </table>
+</div>
 <style>
   .dt-buttons {
     display: inline;
