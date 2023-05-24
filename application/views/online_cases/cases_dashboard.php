256f2c4be90cbbe3465cc0fa9121d5141d5e0aa1
navidaziz98 <navidaziz98@gmail.com>
2023-05-24 22:46:52 +0500

    changes

--- application/views/online_cases/cases_dashboard.php
+++ application/views/online_cases/cases_dashboard.php
@@ -426,7 +426,7 @@
                         AND DATE(note_sheet_completed_date) = '" . $date . "'";
                         $total = $this->db->query($query)->row()->total;
                       ?>
-                        <td style="background-color: rgba(255, 99, 71, <?php echo $total; ?>%);">
+                        <td style="background-color: rgba(255, 0, 0, <?php echo $total; ?>%);">
                           <?php echo $total;  ?>
                         </td>
                       <?php } ?>
