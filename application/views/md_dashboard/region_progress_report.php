2e970b74f67dfac4c8ccb57b76914b8d656f4f5b
navidaziz98 <navidaziz98@gmail.com>
2023-09-21 08:39:05 +0500

    some change in dashbaord

--- application/views/md_dashboard/region_progress_report.php
+++ application/views/md_dashboard/region_progress_report.php
@@ -1,4 +1,47 @@
-<?php $start_time = microtime(true); ?>
+<?php $start_time = microtime(true);
+$query = "SELECT if((`district`.`new_region` = 1),'Central',if((`district`.`new_region` = 2),'South',if((`district`.`new_region` = 3),'Malakand',if((`district`.`new_region` = 4),'Hazara',if((`district`.`new_region` = 5),'Peshawar','Others'))))) AS `region`,
+sum(if(`school`.`file_status` >=1 and school.status>0 ,1,0)) AS `total_applied`,
+sum(if(`school`.`file_status` =3 and school.status=2 ,1,0)) AS `previous_pending`,
+sum(if(`school`.`file_status` = 1 and `school`.`status` = 2,1,0)) AS `total_pending`,
+sum(if((`school`.`reg_type_id` = 1 or `school`.`reg_type_id` = 4) and `school`.`file_status` = 1 and `school`.`status` = 2,1,0)) AS `reg_upg_total`,
+
+sum(if(`school`.`reg_type_id` = 1 and `school`.`file_status` = 1 and `school`.`status` = 2,1,0)) AS `registrations`,
+sum(if(`school`.`reg_type_id` = 1 and `school`.`visit` IS NULL and `school`.`file_status` = 1 and `school`.`status` = 2,1,0)) AS `reg_new`,
+sum(if(`school`.`reg_type_id` = 1 and `school`.`visit` = 'Yes'  and `school`.`file_status` = 1 and `school`.`status` = 2,1,0)) AS `reg_visited_yes`,
+sum(if(`school`.`reg_type_id` = 1 and `school`.`visit` = 'Yes' and `school`.`recommended`='No' and `school`.`file_status` = 1 and `school`.`status` = 2,1,0)) AS `reg_not_recommend`,
+sum(if(`school`.`reg_type_id` = 1 and `school`.`visit` = 'No' and `school`.`file_status` = 1 and `school`.`status` = 2,1,0)) AS `reg_visited_no`,
+
+sum(if(`school`.`reg_type_id` = 4 and `school`.`file_status` = 1 and `school`.`status` = 2,1,0)) AS `renewal_pgradations`,
+sum(if(`school`.`reg_type_id` = 4 and `school`.`visit` IS NULL and `school`.`file_status` = 1 and `school`.`status` = 2,1,0)) AS `re_up_new`,
+sum(if(`school`.`reg_type_id` = 4 and `school`.`visit` = 'Yes'  and `school`.`file_status` = 1 and `school`.`status` = 2,1,0)) AS `re_up_visited_yes`,
+sum(if(`school`.`reg_type_id` = 4 and `school`.`visit` = 'Yes' and `school`.`recommended`='No' and `school`.`file_status` = 1 and `school`.`status` = 2,1,0)) AS `re_up_not_recommend`,
+sum(if(`school`.`reg_type_id` = 4 and `school`.`visit` = 'No' and `school`.`file_status` = 1 and `school`.`status` = 2,1,0)) AS `re_up_visited_no`,
+
+sum(if(`school`.`reg_type_id` = 2 and `school`.`file_status` = 1 and `school`.`status` = 2,1,0)) AS `renewals`,
+
+sum(if(`school`.`reg_type_id` = 3 and `school`.`file_status` = 1 and `school`.`status` = 2,1,0)) AS `upgradations`,
+sum(if(`school`.`file_status` = 5 and `school`.`status` = 2,1,0)) AS `financially_deficient`,
+sum(if(`school`.`file_status` = 5 and `school`.`reg_type_id` = 1 and `school`.`status` = 2,1,0)) AS `reg_deficient`,
+sum(if(`school`.`file_status` = 5 and `school`.`reg_type_id` = 2 and `school`.`status` = 2,1,0)) AS `rene_deficient`,
+sum(if(`school`.`file_status` = 5 and `school`.`reg_type_id` = 4 and `school`.`status` = 2,1,0)) AS `ren_upg_deficient`,
+sum(if(`school`.`file_status` = 4 and `school`.`status` = 2,1,0)) AS `marked_to_operation_wing`,
+sum(if(`school`.`file_status` = 10 and `school`.`status` = 2,1,0)) AS `completed_pending`,
+sum(if(`school`.`status` = 1,1,0)) AS `total_issued`
+from (((`school` 
+join `schools` on(`schools`.`schoolId` = `school`.`schools_id`)) 
+join `district` on(`district`.`districtId` = `schools`.`district_id`)) 
+join `session_year` on(`session_year`.`sessionYearId` = `school`.`session_year_id`)) 
+";
+if ($institute_type_id) {
+    $query .= " AND `schools`.`school_type_id`= '" . $institute_type_id . "' ";
+}
+if ($this->input->post('level_id')) {
+    $level_id = (int) $this->input->post('level_id');
+    $query .= " AND `school`.`level_of_school_id`= '" . $level_id . "' ";
+}
+$query .= " group by `district`.`new_region`";
+$pending_files = $this->db->query($query)->result();
+?>
 
 <strong>Region Wise Progress Summary Report </strong>
 <div class="table-respons ive">
@@ -10,73 +53,40 @@
                 <th colspan="4"></th>
                 <th colspan="5">Registration</th>
                 <th colspan="5">Renewal+Upgradation</th>
+                <th colspan="2"></th>
+                <th colspan="4">Fina. Deficients</th>
             </tr>
             <tr>
                 <th style="text-align: center;">Region</th>
                 <th style="text-align: center;">Applied</th>
                 <th style="text-align: center;">Pending (Queue)</th>
                 <th style="text-align: center;">Total Pending</th>
-                <th>Reg.</th>
-                <th>New Reg.</th>
-                <th>Visit Pending</th>
+                <th>Total</th>
+                <th>New</th>
+                <th>Not Visited</th>
                 <th>Visited</th>
                 <th>Not Recom.</th>
 
-                <th>Ren+Upgr</th>
-                <th>New Ren+Upgr</th>
-                <th>Visit Pending</th>
+                <th>Total</th>
+                <th>New</th>
+                <th>Not Visited</th>
                 <th>Visited</th>
                 <th>Not Recom.</th>
 
-                <td>Ren.+Upgr</td>
                 <td>Upgr</td>
                 <td>Renewal</td>
-                <td>Fin.Deficients</td>
+                <td>Total</td>
+                <td>Regi</td>
+                <td>Ren+Upg</td>
+                <td>Renewal</td>
                 <td>(10%)</td>
-                <td>Issue Pending</td>
+                <td>Issue Pend.</td>
             </tr>
         </thead>
 
         <tbody>
 
             <?php
-            $query = "SELECT if((`district`.`new_region` = 1),'Central',if((`district`.`new_region` = 2),'South',if((`district`.`new_region` = 3),'Malakand',if((`district`.`new_region` = 4),'Hazara',if((`district`.`new_region` = 5),'Peshawar','Others'))))) AS `region`,
-            sum(if(`school`.`file_status` >=1 and school.status>0 ,1,0)) AS `total_applied`,
-            sum(if(`school`.`file_status` =3 and school.status=2 ,1,0)) AS `previous_pending`,
-            sum(if(`school`.`file_status` = 1 and `school`.`status` = 2,1,0)) AS `total_pending`,
-            sum(if((`school`.`reg_type_id` = 1 or `school`.`reg_type_id` = 4) and `school`.`file_status` = 1 and `school`.`status` = 2,1,0)) AS `reg_upg_total`,
-
-            sum(if(`school`.`reg_type_id` = 1 and `school`.`file_status` = 1 and `school`.`status` = 2,1,0)) AS `registrations`,
-            sum(if(`school`.`reg_type_id` = 1 and `school`.`visit` IS NULL and `school`.`file_status` = 1 and `school`.`status` = 2,1,0)) AS `reg_new`,
-            sum(if(`school`.`reg_type_id` = 1 and `school`.`visit` = 'Yes' and `school`.`recommended`='No' and `school`.`file_status` = 1 and `school`.`status` = 2,1,0)) AS `reg_not_recommend`,
-            sum(if(`school`.`reg_type_id` = 1 and `school`.`visit` = 'No' and `school`.`file_status` = 1 and `school`.`status` = 2,1,0)) AS `reg_visited_no`,
-
-            sum(if(`school`.`reg_type_id` = 4 and `school`.`file_status` = 1 and `school`.`status` = 2,1,0)) AS `renewal_pgradations`,
-            sum(if(`school`.`reg_type_id` = 4 and `school`.`visit` IS NULL and `school`.`file_status` = 1 and `school`.`status` = 2,1,0)) AS `re_up_new`,
-            sum(if(`school`.`reg_type_id` = 4 and `school`.`visit` = 'Yes' and `school`.`recommended`='No' and `school`.`file_status` = 1 and `school`.`status` = 2,1,0)) AS `re_up_not_recommend`,
-            sum(if(`school`.`reg_type_id` = 4 and `school`.`visit` = 'No' and `school`.`file_status` = 1 and `school`.`status` = 2,1,0)) AS `re_up_visited_no`,
-
-            sum(if(`school`.`reg_type_id` = 2 and `school`.`file_status` = 1 and `school`.`status` = 2,1,0)) AS `renewals`,
-            
-            sum(if(`school`.`reg_type_id` = 3 and `school`.`file_status` = 1 and `school`.`status` = 2,1,0)) AS `upgradations`,
-            sum(if(`school`.`file_status` = 5 and `school`.`status` = 2,1,0)) AS `financially_deficient`,
-            sum(if(`school`.`file_status` = 4 and `school`.`status` = 2,1,0)) AS `marked_to_operation_wing`,
-            sum(if(`school`.`file_status` = 10 and `school`.`status` = 2,1,0)) AS `completed_pending`,
-            sum(if(`school`.`status` = 1,1,0)) AS `total_issued`
-            from (((`school` 
-            join `schools` on(`schools`.`schoolId` = `school`.`schools_id`)) 
-            join `district` on(`district`.`districtId` = `schools`.`district_id`)) 
-            join `session_year` on(`session_year`.`sessionYearId` = `school`.`session_year_id`)) 
-            ";
-            if ($institute_type_id) {
-                $query .= " AND `schools`.`school_type_id`= '" . $institute_type_id . "' ";
-            }
-            if ($this->input->post('level_id')) {
-                $level_id = (int) $this->input->post('level_id');
-                $query .= " AND `school`.`level_of_school_id`= '" . $level_id . "' ";
-            }
-            $query .= " group by `district`.`new_region`";
-            $pending_files = $this->db->query($query)->result();
             foreach ($pending_files as $pending) { ?>
                 <tr>
                     <th style="text-align: center;"><?php echo $pending->region; ?></th>
@@ -99,6 +109,9 @@
                     <td><?php echo $pending->upgradations; ?></td>
                     <td><?php echo $pending->renewals; ?></td>
                     <td><?php echo $pending->financially_deficient; ?></td>
+                    <td><?php echo $pending->reg_deficient; ?></td>
+                    <td><?php echo $pending->ren_upg_deficient; ?></td>
+                    <td><?php echo $pending->rene_deficient; ?></td>
                     <td><?php echo $pending->marked_to_operation_wing; ?></td>
                     <td><?php echo $pending->completed_pending; ?></td>
 
