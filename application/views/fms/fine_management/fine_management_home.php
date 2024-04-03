e92ef4c685e6987a05a01479e7da52e0aeeb9801
navidaziz98 <navidaziz98@gmail.com>
2024-04-04 02:14:05 +0500

    some changes

--- application/views/fms/fine_management/fine_management_home.php
+++ application/views/fms/fine_management/fine_management_home.php
@@ -1,11 +1,11 @@
-<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.24/css/jquery.dataTables.css">
-<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.js"></script>
-<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/buttons/2.0.0/js/dataTables.buttons.min.js"></script>
-<script type="text/javascript" charset="utf8" src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
-<script type="text/javascript" charset="utf8" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
-<script type="text/javascript" charset="utf8" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
-<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/buttons/2.0.0/js/buttons.html5.min.js"></script>
-<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/buttons/2.0.0/js/buttons.print.min.js"></script>
+<link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.css" />
+<script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.3.6/js/dataTables.buttons.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.3.6/js/buttons.html5.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.3.6/js/buttons.print.min.js"></script>
 
 
 <div class="content-wrapper">
@@ -185,5 +185,39 @@
 	$(document).ready(function() {
 		fined_school_list();
 		fined_summary();
+	});
+</script>
+
+<script>
+	title = "<?php echo "Fine Management Data: Date:" . date("d M, Y h:m:s"); ?>";
+	$(document).ready(function() {
+		$('#ajax_fine_list').DataTable({
+			dom: 'Bfrtip',
+			paging: false,
+			title: title,
+			"order": [],
+			"ordering": false,
+			searching: true,
+			buttons: [
+
+				{
+					extend: 'print',
+					title: title,
+
+				},
+				{
+					extend: 'excelHtml5',
+					title: title,
+
+				},
+				{
+					extend: 'pdfHtml5',
+					title: title,
+					pageSize: 'A4',
+					orientation: 'landscape',
+
+				}
+			]
+		});
 	});
 </script>
